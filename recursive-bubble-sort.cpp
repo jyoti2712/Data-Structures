dafc400a50b2afb6d83ed3f05ee46b72cd71dabe
Jyoti Panda <67140161+jyoti2712@users.noreply.github.com>
2024-10-31 14:45:06 +0530

    Update recursive-bubble-sort.cpp
    changed the code to be more efficient

--- recursive-bubble-sort.cpp
+++ recursive-bubble-sort.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void bubble_sort(int arr[], int i, int n){
+void bubble_sort(int arr[], int i){
     if(i==0) return;
     for(int j = 0; j<=i-1; j++){
         if(arr[j] > arr[j+1]){
@@ -10,14 +10,14 @@ void bubble_sort(int arr[], int i, int n){
             arr[j] = temp;
         }
     }
-    bubble_sort(arr, i-1, n);
+    bubble_sort(arr, i-1);
 }
 
 int main(){
     int arr[] = {4, 6, 2, 5, 7, 9, 1, 3};
     int n = sizeof(arr) / sizeof(arr[0]);
     int i = n -1;
-    bubble_sort(arr, i, n);
+    bubble_sort(arr, i);
     for(int i = 0; i < n; i++){
         cout<< arr[i] << " ";
     }
