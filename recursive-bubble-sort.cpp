1095d694c77d736f9bcf0fe98e6b87efa330925a
Jyoti Panda <67140161+jyoti2712@users.noreply.github.com>
2024-10-31 14:21:48 +0530

    Create recursive-bubble-sort
    cpp code for bubble sort

---
+++ recursive-bubble-sort.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int arr[], int i, int n){
+    if(i==0) return;
+    for(int j = 0; j<=i-1; j++){
+        if(arr[j] > arr[j+1]){
+            int temp = arr[j+1];
+            arr[j+1] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    bubble_sort(arr, i-1, n);
+}
+
+int main(){
+    int arr[] = {4, 6, 2, 5, 7, 9, 1, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int i = n -1;
+    bubble_sort(arr, i, n);
+    for(int i = 0; i < n; i++){
+        cout<< arr[i] << " ";
+    }
+    cout<<endl;
+    return 0;
+}
